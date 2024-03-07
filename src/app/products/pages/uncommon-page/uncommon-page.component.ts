25d3f1e4c911b988d89ee7aba8a397c17d9e7fff
Mariana <macbedoya@gmail.com>
2024-03-07 10:40:13 -0500

    i18nSelect

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.scss'
 })
 export class UncommonPageComponent {
+  //i18nSelect
+  name:string = 'Mariana'
+  gender: 'female' | 'male' = 'female';
+  invitationMap = {
+    'female': 'invitarla',
+    'male': 'invitarlo'
+  }
+  changeClient () {
+    this.name = 'Carlos';
+    this.gender = 'male';
+  }
 
+  
 }
